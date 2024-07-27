fd955ddc19f5bcd0167b78bfc43145664642bf07
Koen Romers <koenoe@users.noreply.github.com>
2024-07-27 14:08:04 +0100

    chore: setup tooling

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    ppr: true,
+    staleTimes: {
+      dynamic: 30,
+      static: 180,
+    },
+  },
+};
 
 export default nextConfig;

83550eb8670049763392530218519e5929592ce1
Koen Romers <koenoe@users.noreply.github.com>
2024-07-31 21:31:25 +0100

    chore: add speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
+import './globals.css';
+
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import './globals.css';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import Header from '@/components/Header';
 
 const inter = Inter({ subsets: ['latin'], display: 'swap' });
@@ -23,6 +25,7 @@ export default function RootLayout({
         <Header />
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );

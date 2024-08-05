0c6940563a3025cb92f5a3b57c77ac018c908c58
Koen Romers <koenoe@users.noreply.github.com>
2024-08-05 16:07:34 +0100

    fix: scrollbar detection

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,27 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={cx('flex min-h-screen flex-col', inter.className)}>
+        <script
+          // eslint-disable-next-line react/no-danger
+          dangerouslySetInnerHTML={{
+            __html: `
+              const scrollable = document.createElement("div");
+              scrollable.style.overflow = "scroll";
+              scrollable.style.width = "10px";
+              scrollable.style.height = "10px";
+              scrollable.style.position = "absolute";
+              scrollable.style.top = "0px";
+              scrollable.style.visibility = "hidden";
+              if (document.body) {
+                document.body.appendChild(scrollable);
+                if (scrollable.scrollWidth !== scrollable.offsetWidth) {
+                  document.body.classList.add("scrollbar-is-visible");
+                }
+                document.body.removeChild(scrollable);
+              }
+            `,
+          }}
+        />
         <Header />
         {children}
         <Footer />

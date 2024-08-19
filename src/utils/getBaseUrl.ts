63ec363b55bfc8a60331d8875e710dc3d39bd185
Koen Romers <koenoe@users.noreply.github.com>
2024-08-19 20:05:18 +0100

    fix: getBaseUrl

--- src/utils/getBaseUrl.ts
+++ src/utils/getBaseUrl.ts
@@ -1,7 +1,16 @@
 import 'server-only';
 
 export default function getBaseUrl() {
-  return process.env.VERCEL_URL
-    ? `https://${process.env.VERCEL_URL}`
-    : 'http://localhost:3000';
+  if (process.env.VERCEL_ENV === 'production') {
+    return `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`;
+  } else if (
+    process.env.VERCEL_ENV === 'preview' &&
+    process.env.VERCEL_BRANCH_URL
+  ) {
+    return `https://${process.env.VERCEL_BRANCH_URL}`;
+  } else if (process.env.VERCEL_URL) {
+    return `https://${process.env.VERCEL_URL}`;
+  } else {
+    return 'http://localhost:3000';
+  }
 }

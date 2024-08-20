5032ff0fe2d068845146d5d0527cabec0e452dc9
Koen Romers <koenoe@users.noreply.github.com>
2024-08-20 14:00:32 +0100

    Update LoginButtonContainer.tsx

--- src/components/Buttons/LoginButtonContainer.tsx
+++ src/components/Buttons/LoginButtonContainer.tsx
@@ -1,3 +1,4 @@
+import { unstable_noStore as noStore } from 'next/cache';
 import { cookies } from 'next/headers';
 
 import { fetchAccountDetails } from '@/lib/tmdb';
@@ -6,8 +7,11 @@ import { decryptToken } from '@/lib/token';
 import LoginButton from './LoginButton';
 
 export default async function LoginButtonContainer() {
-  const cookieStore = cookies();
-  const encryptedSessionId = cookieStore.get('sessionId')?.value;
+  // FIXME: this shouldn't be needed, but somehow this component is still cached
+  // when it shouldn't be. Investigate later.
+  noStore();
+
+  const encryptedSessionId = cookies().get('sessionId')?.value;
 
   if (encryptedSessionId) {
     const decryptedSessionId = decryptToken(encryptedSessionId);

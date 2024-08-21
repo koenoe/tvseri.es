08acfd624f3608a0a7741488bc8e5fcc40d4b3d2
Koen Romers <koenoe@users.noreply.github.com>
2024-08-21 23:10:51 +0100

    chore: remove unneeded fallback on suspense

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,14 @@ export default function Header() {
     <div className="absolute h-[6rem] w-screen md:h-[8rem]">
       <div className="container flex h-full w-full items-center justify-stretch">
         <Logo priority />
-        <Suspense>
+        <Suspense
+          fallback={
+            <div className="relative z-10 ml-auto flex gap-5">
+              <div className="h-[24px] w-[24px] animate-pulse bg-white/30" />
+              <div className="h-[24px] w-[24px] animate-pulse bg-white/30" />
+            </div>
+          }
+        >
           <MenuContainer />
         </Suspense>
       </div>
--- src/components/Menu/MenuContainer.tsx
+++ src/components/Menu/MenuContainer.tsx
@@ -5,13 +5,13 @@ import { cookies } from 'next/headers';
 import Menu from './Menu';
 import Username from './Username';
 
-export default function MenuContainer() {
+export default async function MenuContainer() {
   const isAuthenticated = !!cookies().get('sessionId')?.value;
 
   return (
     <Menu isAuthenticated={isAuthenticated}>
       <Suspense
-        fallback={<div className="h-[18px] w-14 animate-pulse bg-white/30" />}
+        fallback={<div className="h-[18px] w-16 animate-pulse bg-white/30" />}
       >
         <Username />
       </Suspense>

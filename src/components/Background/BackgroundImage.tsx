43ded6eb658a32545242bdb9355ecfba360f6c14
Koen Romers <koenoe@users.noreply.github.com>
2024-09-04 16:32:26 +0100

    Update BackgroundImage.tsx

--- src/components/Background/BackgroundImage.tsx
+++ src/components/Background/BackgroundImage.tsx
@@ -1,4 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
+import { preload } from 'react-dom';
+
 export default function BackgroundImage({
   src = 'data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==',
   ...rest
@@ -8,19 +10,31 @@ export default function BackgroundImage({
     'w1920_and_h1080_multi_faces',
     'w1280_and_h720_multi_faces',
   );
+  const imageSizes = '100vw';
+  const imageSrcSet = `
+    ${SD} 768w,
+    ${HD} 1200w
+  `;
+
+  preload(SD, {
+    as: 'image',
+    fetchPriority: 'high',
+    imageSrcSet,
+    imageSizes,
+  });
 
   return (
-    <img
-      className="h-full w-full object-cover object-top"
-      src={SD}
-      alt=""
-      draggable={false}
-      srcSet={`
-        ${SD} 768w,
-        ${HD} 1200w
-    `}
-      sizes="100vw"
-      {...rest}
-    />
+    <>
+      <img
+        className="h-full w-full object-cover object-top"
+        decoding="async"
+        src={SD}
+        alt=""
+        draggable={false}
+        srcSet={imageSrcSet}
+        sizes={imageSizes}
+        {...rest}
+      />
+    </>
   );
 }

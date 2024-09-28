a8d330b434d8a220194e35b58e2c37e2e5b47f60
Koen Romers <koenoe@users.noreply.github.com>
2024-09-28 10:00:26 +0100

    chore: tweaks

--- src/app/tv/[id]/[[...slug]]/loading.tsx
+++ src/app/tv/[id]/[[...slug]]/loading.tsx
@@ -7,7 +7,7 @@ export default function Loading() {
   return (
     <SkeletonPage>
       <div className="container animate-pulse">
-        <div className="relative flex h-[calc(85vh-16rem)] items-end md:h-[calc(60vh-8rem)]">
+        <div className="relative flex h-[calc(85vh-16rem)] items-end md:h-[calc(65vh-8rem)]">
           <div className="w-full xl:w-4/5 2xl:w-3/5">
             <div className="relative mb-6 h-28 w-full bg-white/5 md:h-40 md:w-3/5" />
             <div className="mb-6 h-6 w-4/5 bg-white/10" />

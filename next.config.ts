b0934f78d5d81230cb0755cb6caa1bb7f29f5e17
Koen Romers <koenoe@users.noreply.github.com>
2024-11-05 19:39:20 +0000

    feat(sst): bump

--- next.config.ts
+++ next.config.ts
@@ -6,9 +6,8 @@ const nextConfig = {
     // TODO: figure out what broke in latest canary as searchParams aren't working properly anymore
     ppr: false,
     staleTimes: {
-      // TODO: there's a bug with searchParams atm
-      dynamic: 0, // default is 30
-      static: 0, // default is 180
+      dynamic: 0,
+      static: 300,
     },
   },
   images: {

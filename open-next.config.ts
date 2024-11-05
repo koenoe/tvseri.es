88918095afdd6eb1703bd9ba716d9fc7bc9815b2
Koen Romers <koenoe@users.noreply.github.com>
2024-11-05 20:14:47 +0000

    Update open-next.config.ts

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,5 +1,8 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
+  dangerous: {
+    enableCacheInterception: true,
+  },
   default: {
     override: {
       wrapper: 'aws-lambda-streaming',

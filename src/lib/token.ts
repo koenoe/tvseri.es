ae76fd34617e8468c297c86db512718c30b2fd8c
Koen Romers <koenoe@users.noreply.github.com>
2024-11-12 14:42:56 +0000

    chore: add linting workflow

--- src/lib/token.ts
+++ src/lib/token.ts
@@ -6,7 +6,9 @@ const key = Buffer.from(String(process.env.SECRET_KEY), 'base64');
 
 export function encryptToken(plaintext: string) {
   const iv = crypto.randomBytes(16);
+  // @ts-ignore
   const cipher = crypto.createCipheriv('aes-256-cbc', key, iv);
+  // @ts-ignore
   const encrypted = Buffer.concat([
     iv,
     cipher.update(plaintext, 'utf8'),
@@ -19,7 +21,9 @@ export function decryptToken(ivCiphertextB64: string) {
   const ivCiphertext = Buffer.from(ivCiphertextB64, 'base64url');
   const iv = ivCiphertext.subarray(0, 16);
   const ciphertext = ivCiphertext.subarray(16);
+  // @ts-ignore
   const cipher = crypto.createDecipheriv('aes-256-cbc', key, iv);
+  // @ts-ignore
   const decrypted = Buffer.concat([cipher.update(ciphertext), cipher.final()]);
   return decrypted.toString('utf-8');
 }

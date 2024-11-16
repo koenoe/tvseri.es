dc6f72f556ba173263003719efbcaf02fd759408
Koen Romers <koenoe@users.noreply.github.com>
2024-11-16 21:52:52 +0000

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,6 @@ import getBaseUrl from './src/utils/getBaseUrl';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    // TODO: figure out what broke in latest canary as searchParams aren't working properly anymore
-    ppr: false,
-    staleTimes: {
-      // TODO: there's a bug with searchParams atm
-      dynamic: 0, // default is 30
-      static: 0, // default is 180
-    },
-  },
   images: {
     deviceSizes: [640, 1200, 1920],
     imageSizes: [128, 256, 384],

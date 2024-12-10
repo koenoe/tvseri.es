109b7605c84cd971c558d4d7b6cf01472e327171
Koen Romers <koenoe@users.noreply.github.com>
2024-12-10 20:03:53 +0000

    Update sst.config.ts

--- sst.config.ts
+++ sst.config.ts
@@ -8,7 +8,6 @@ export default $config({
       home: 'aws',
       providers: {
         aws: {
-          version: '6.64.0',
           profile:
             input.stage === 'production'
               ? 'tvseries-production'

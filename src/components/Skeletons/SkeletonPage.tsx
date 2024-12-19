6c106ac96561b441ee9bb6753c9fdd2d837e81d4
Koen Romers <koenoe@users.noreply.github.com>
2024-12-19 14:09:40 +0000

    Update SkeletonPage.tsx

--- src/components/Skeletons/SkeletonPage.tsx
+++ src/components/Skeletons/SkeletonPage.tsx
@@ -1,11 +1,19 @@
+'use client';
+
+import { useEffect } from 'react';
+
 export default function SkeletonPage({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
-    <main className="grow scroll-mt-[6rem] pb-20 pt-[6rem] md:scroll-mt-[8rem] md:pt-[8rem]">
-      <div className="relative z-10">{children}</div>
+    <main className="grow scroll-mt-[6rem] pb-20 pt-[6rem] transition-colors duration-500 md:scroll-mt-[8rem] md:pt-[8rem]">
+      {children}
     </main>
   );
 }

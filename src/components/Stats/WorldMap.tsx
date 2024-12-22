e31261395452ab9446028c5a125730973e4ce8d6
Koen Romers <koenoe@users.noreply.github.com>
2024-12-22 13:23:29 +0000

    feat(stats): world map

--- src/components/Stats/WorldMap.tsx
+++ src/components/Stats/WorldMap.tsx
@@ -4,6 +4,38 @@ import { cx } from 'class-variance-authority';
 
 import WorldMap from '../WorldMap/WorldMap';
 
+function generateCountryData(data: Record<string, number>) {
+  const maxViews = Math.max(...Object.values(data));
+  const countryData: Record<
+    string,
+    { color: string; hoverColor: string; content: { views: number } }
+  > = {};
+  const minOpacity = 0.3;
+
+  Object.entries(data).forEach(([country, views]) => {
+    const opacity = minOpacity + (views / maxViews) * (1 - minOpacity);
+    countryData[country] = {
+      color: `rgba(255, 0, 128, ${opacity})`,
+      hoverColor: '#00B8D4',
+      content: { views },
+    };
+  });
+
+  return countryData;
+}
+
+const dummyData = {
+  'United States': 20,
+  'United Kingdom': 5,
+  France: 1,
+  Australia: 1,
+  Japan: 3,
+  Spain: 1,
+  Netherlands: 2,
+  Sweden: 1,
+  'South Korea': 5,
+};
+
 export default function WorldMapForStats({
   className,
 }: Readonly<{
@@ -16,7 +48,28 @@ export default function WorldMapForStats({
         <div className="h-[2px] flex-grow bg-white/10" />
       </div>
       <div className="flex items-center justify-center">
-        <WorldMap className="w-3/4" hoverColor="#00B8D4" />
+        <WorldMap
+          className="w-full md:w-3/4"
+          data={generateCountryData(dummyData)}
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          renderTooltip={({ country, content, hoverColor }: any) => (
+            <div className="w-40 rounded-lg border-0 bg-neutral-900 px-4 py-2 text-xs">
+              <div className="mb-1 font-medium text-white">{country}</div>
+              <div className="flex items-center gap-1">
+                <div
+                  className="mr-1 h-3 w-3 rounded-sm"
+                  style={{
+                    backgroundColor: hoverColor,
+                  }}
+                />
+                <span className="text-zinc-400">Series</span>
+                <span className="ml-auto font-medium text-white">
+                  {content?.views ?? 0}
+                </span>
+              </div>
+            </div>
+          )}
+        />
       </div>
     </div>
   );
--- src/components/WorldMap/WorldMap.tsx
+++ src/components/WorldMap/WorldMap.tsx
@@ -1,26 +1,54 @@
 // Note: heavily inspired by https://github.com/react-map/react-map/blob/master/packages/world/src/World.tsx
 'use client';
 
-import { useEffect, useId, useState } from 'react';
+import { type ReactNode, useCallback, useEffect, useId, useState } from 'react';
 
 import { cx } from 'class-variance-authority';
-import { motion } from 'framer-motion';
+import { motion, AnimatePresence } from 'framer-motion';
 
 import { countries, paths } from './constants';
+
+type CountryData = Readonly<{
+  color: string;
+  hoverColor?: string;
+  strokeColor?: string;
+  content?: unknown;
+}>;
 
 export default function WorldMap({
   className,
   defaultColor = '#333333',
-  hoverColor = '#444444',
-  strokeColor = '#222222',
+  defaultHoverColor = '#444444',
+  defaultStrokeColor = '#222222',
+  data = {},
+  renderTooltip,
 }: Readonly<{
   className?: string;
   defaultColor?: string;
-  hoverColor?: string;
-  strokeColor?: string;
+  defaultHoverColor?: string;
+  defaultStrokeColor?: string;
+  data?: Record<string, CountryData>;
+  renderTooltip?: (
+    payload: Readonly<{
+      country: string;
+      color: string;
+      hoverColor: string;
+      strokeColor: string;
+      content: unknown;
+    }>,
+  ) => ReactNode;
 }>) {
   const instanceId = useId().replace(/:/g, '');
   const [viewBox, setViewBox] = useState<string>('0 0 100 100');
+  const [tooltipData, setTooltipData] = useState<{
+    country: string;
+    color: string;
+    hoverColor: string;
+    strokeColor: string;
+    content: unknown;
+    x: number;
+    y: number;
+  } | null>(null);
 
   useEffect(() => {
     const svg = document.getElementById(
@@ -33,33 +61,88 @@ export default function WorldMap({
     }
   }, [instanceId]);
 
+  const handleMouseMove = useCallback(
+    (e: React.MouseEvent, country: string) => {
+      const countryData = data[country];
+
+      const svgElement = document.getElementById(`svg2-${instanceId}`);
+      if (!svgElement) {
+        return;
+      }
+
+      const rect = svgElement.getBoundingClientRect();
+      setTooltipData({
+        country,
+        color: countryData?.color ?? defaultColor,
+        hoverColor: countryData?.hoverColor ?? defaultHoverColor,
+        strokeColor: countryData?.strokeColor ?? defaultStrokeColor,
+        content: countryData?.content ?? null,
+        x: e.clientX - rect.left + 10, // little offset
+        y: e.clientY - rect.top + 10, // little offset
+      });
+    },
+    [data, defaultColor, defaultHoverColor, defaultStrokeColor, instanceId],
+  );
+
+  const handleMouseLeave = useCallback(() => {
+    setTooltipData(null);
+  }, []);
+
   return (
-    <>
-      <div className={cx('relative w-full', className)}>
-        <svg
-          className="aspect-video"
-          version="1.1"
-          id={`svg2-${instanceId}`}
-          x="0px"
-          y="0px"
-          viewBox={viewBox}
-        >
-          {countries?.map((name, index) => (
-            <motion.path
-              key={index}
-              // onClick={() => handleClick(code)}
-              id={`${name}-${instanceId}`}
-              d={paths[name as keyof typeof paths]}
-              style={{
-                strokeWidth: 1,
-                stroke: strokeColor,
-              }}
-              initial={{ fill: defaultColor }}
-              whileHover={{ fill: hoverColor }}
-            />
-          ))}
-        </svg>
-      </div>
-    </>
+    <div className={cx('relative w-full', className)}>
+      <svg
+        className="aspect-video"
+        version="1.1"
+        id={`svg2-${instanceId}`}
+        x="0px"
+        y="0px"
+        viewBox={viewBox}
+      >
+        {countries?.map((name, index) => (
+          <motion.path
+            key={index}
+            id={`${name}-${instanceId}`}
+            d={paths[name as keyof typeof paths]}
+            style={{
+              strokeWidth: 1,
+              stroke: data[name]?.strokeColor ?? defaultStrokeColor,
+              cursor: 'pointer',
+            }}
+            initial={{ fill: data[name]?.color ?? defaultColor }}
+            whileHover={{ fill: data[name]?.hoverColor ?? defaultHoverColor }}
+            onMouseMove={(e) => handleMouseMove(e, name)}
+            onMouseLeave={handleMouseLeave}
+          />
+        ))}
+      </svg>
+
+      {renderTooltip && tooltipData && (
+        <AnimatePresence>
+          <motion.div
+            className="pointer-events-none absolute left-0 top-0"
+            initial={{ opacity: 0, x: tooltipData.x - 20, y: tooltipData.y }}
+            animate={{
+              opacity: 1,
+              x: tooltipData.x,
+              y: tooltipData.y,
+            }}
+            exit={{ opacity: 0 }}
+            transition={{
+              type: 'tween',
+              duration: 0.1,
+              ease: 'linear',
+            }}
+          >
+            {renderTooltip({
+              country: tooltipData.country,
+              color: tooltipData.color,
+              hoverColor: tooltipData.hoverColor,
+              strokeColor: tooltipData.strokeColor,
+              content: tooltipData.content,
+            })}
+          </motion.div>
+        </AnimatePresence>
+      )}
+    </div>
   );
 }

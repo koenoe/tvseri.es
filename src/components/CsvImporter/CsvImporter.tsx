05faf76540193a87177bd97b38bc9a4e590d5aa0
Koen Romers <koenoe@users.noreply.github.com>
2025-01-02 10:45:26 +0000

    Update CsvImporter.tsx

--- src/components/CsvImporter/CsvImporter.tsx
+++ src/components/CsvImporter/CsvImporter.tsx
@@ -65,20 +65,23 @@ export default function CsvImporter({
           <div className="ml-auto h-11 w-24 rounded-3xl bg-white/5" />
         </div>
 
-        <Table className="h-[calc(100vh-28.25rem)] text-xs">
+        <Table className="h-[calc(100vh-29rem)] text-xs">
           <TableHeader className="sticky top-0 z-10 border-b">
             <TableRow>
               {Array(fields.length)
                 .fill(null)
                 .map((_, index) => (
                   <TableHead key={index}>
-                    <div className="h-9 w-48 rounded bg-white/5" />
+                    <div className="flex items-center gap-x-4">
+                      <div className="h-4 w-24 bg-white/10" />
+                      <div className="ml-auto h-9 w-48 rounded bg-white/5" />
+                    </div>
                   </TableHead>
                 ))}
             </TableRow>
           </TableHeader>
           <TableBody>
-            {Array(20)
+            {Array(50)
               .fill(null)
               .map((_, rowIndex) => (
                 <TableRow key={rowIndex} className="h-9">
@@ -124,7 +127,7 @@ export default function CsvImporter({
           <span>Reset</span>
         </button>
       </div>
-      <Table className="h-[calc(100vh-28.25rem)] text-xs">
+      <Table className="h-[calc(100vh-29rem)] text-xs">
         <TableHeader className="sticky top-0 z-10 border-b">
           <TableRow>
             {fields.map((field) => (

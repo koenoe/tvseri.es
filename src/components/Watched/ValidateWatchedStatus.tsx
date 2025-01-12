e5fee9343cef3a65aaa5dd7629a8d4422e762491
Koen Romers <koenoe@users.noreply.github.com>
2025-01-12 22:06:02 +0000

    fix(watched): validate status and cleanup

--- src/components/Watched/ValidateWatchedStatus.tsx
+++ src/components/Watched/ValidateWatchedStatus.tsx
@@ -1,11 +1,13 @@
 import auth from '@/lib/auth';
 import { cachedTvSeries } from '@/lib/cached';
 import { addToList, isInList, removeFromList } from '@/lib/db/list';
-import {
-  getLastWatchedItemForTvSeries,
-  isTvSeriesWatched,
-} from '@/lib/db/watched';
+import { getWatchedCountForTvSeries } from '@/lib/db/watched';
 
+// Note: The DynamoDB subscriber (`src/lambdas/watched.ts`) handles marking a TV series as watched or in progress.
+// However, a series marked as watched may later have new episodes added.
+// In such cases, the series should be moved from the "watched" list to the "in progress" list.
+// Automating this process in the future with a cron job or similar mechanism could be beneficial.
+// For now, this is handled when navigating to the TV series page, which renders this component.
 export default async function ValidateWatchedStatus({
   id,
 }: Readonly<{
@@ -21,51 +23,39 @@ export default async function ValidateWatchedStatus({
     return null;
   }
 
-  const [tvSeriesIsWatched, isInWatchedList, lastWatchedItem] =
-    await Promise.all([
-      isTvSeriesWatched({
+  const [tvSeriesIsWatched, isInWatchedList] = await Promise.all([
+    async () => {
+      const watchedCount = await getWatchedCountForTvSeries({
         userId: user.id,
         tvSeries,
-      }),
-      isInList({
-        userId: user.id,
-        listId: 'WATCHED',
-        id: tvSeries.id,
-      }),
-      getLastWatchedItemForTvSeries({
-        userId: user.id,
-        tvSeries,
-      }),
-    ]);
+      });
+      return watchedCount === tvSeries.numberOfEpisodes;
+    },
+    isInList({
+      userId: user.id,
+      listId: 'WATCHED',
+      id: tvSeries.id,
+    }),
+  ]);
 
-  if (!isInWatchedList && tvSeriesIsWatched) {
+  if (isInWatchedList && !tvSeriesIsWatched) {
     await Promise.all([
       addToList({
         userId: user.id,
-        listId: 'WATCHED',
+        listId: 'IN_PROGRESS',
         item: {
           id: tvSeries.id,
           title: tvSeries.title,
           slug: tvSeries.slug,
           posterPath: tvSeries.posterPath,
-          createdAt: lastWatchedItem?.watchedAt,
         },
       }),
       removeFromList({
         userId: user.id,
-        listId: 'IN_PROGRESS',
+        listId: 'WATCHED',
         id: tvSeries.id,
       }),
     ]);
-    return null;
-  }
-
-  if (!tvSeriesIsWatched) {
-    await removeFromList({
-      userId: user.id,
-      listId: 'WATCHED',
-      id: tvSeries.id,
-    });
   }
 
   return null;

a41bbe6cd1f946bb8505da1368a6e9a52400d990
Koen Romers <koenoe@users.noreply.github.com>
2025-01-14 17:10:20 +0000

    chore: bumps

--- sst-env.d.ts
+++ sst-env.d.ts
@@ -2,8 +2,7 @@
 /* tslint:disable */
 /* eslint-disable */
 /* deno-fmt-ignore-file */
-import "sst"
-export {}
+
 declare module "sst" {
   export interface Resource {
     "Cache": {
@@ -57,3 +56,7 @@ declare module "sst" {
     }
   }
 }
+/// <reference path="sst-env.d.ts" />
+
+import "sst"
+export {}

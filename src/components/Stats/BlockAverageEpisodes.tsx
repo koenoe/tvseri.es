8620724a6238483067965d0e7ba94423955d4746
Koen Romers <koenoe@users.noreply.github.com>
2025-01-15 14:12:23 +0000

    Update BlockAverageEpisodes.tsx

--- src/components/Stats/BlockAverageEpisodes.tsx
+++ src/components/Stats/BlockAverageEpisodes.tsx
@@ -3,9 +3,22 @@ import calculatePercentageDelta from '@/utils/calculatePercentageDelta';
 
 import Block from './Block';
 
-function calculateDailyAverage(count: number, year: number): number {
-  const daysInYear = year % 4 === 0 ? 366 : 365;
-  return Math.round((count / daysInYear) * 10) / 10;
+function calculateDailyAverage(
+  count: number,
+  year: number,
+  currentDate = new Date(),
+): number {
+  if (year < currentDate.getFullYear()) {
+    const daysInYear = year % 4 === 0 ? 366 : 365;
+    return Math.round((count / daysInYear) * 10) / 10;
+  }
+
+  const startOfYear = new Date(year, 0, 1);
+  const daysDiff =
+    Math.floor(
+      (currentDate.getTime() - startOfYear.getTime()) / (1000 * 60 * 60 * 24),
+    ) + 1;
+  return Math.round((count / daysDiff) * 10) / 10;
 }
 
 export default async function BlockAverageEpisodes({

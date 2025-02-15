66e334d778d5d61d565ba2931ab252d8df5ed232
Koen Romers <koenoe@users.noreply.github.com>
2025-02-15 21:23:01 +0000

    Update preloadImage.ts

--- src/utils/preloadImage.ts
+++ src/utils/preloadImage.ts
@@ -1,8 +1,17 @@
+const preloadCache = new Map<string, string>();
+
 export default function preloadImage(src: string): Promise<string> {
+  if (preloadCache.has(src)) {
+    return Promise.resolve(preloadCache.get(src)!);
+  }
+
   return new Promise((resolve, reject) => {
     const img = new Image();
     img.src = src;
-    img.onload = () => resolve(src);
+    img.onload = () => {
+      preloadCache.set(src, src);
+      resolve(src);
+    };
     img.onerror = (err) => reject(err);
   });
 }

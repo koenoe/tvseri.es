f241f4f4b77bf3ebff936844aa535e906388b470
Koen Romers <koenoe@users.noreply.github.com>
2025-05-01 20:51:50 +0100

    Update Cast.tsx

--- src/components/Cast/Cast.tsx
+++ src/components/Cast/Cast.tsx
@@ -1,8 +1,27 @@
-import { unstable_cacheLife as cacheLife } from 'next/cache';
+import { cache } from 'react';
 
+import { getCacheItem, setCacheItem } from '@/lib/db/cache';
 import { fetchTvSeriesCredits } from '@/lib/tmdb';
+import { type Person } from '@/types/person';
 
 import Avatars from '../Avatars/Avatars';
+
+const cachedCast = cache(async (id: number) => {
+  const dynamoCacheKey = `cast:${id}`;
+  const dynamoCachedItem = await getCacheItem<Person[]>(dynamoCacheKey);
+  if (dynamoCachedItem) {
+    return dynamoCachedItem;
+  }
+
+  const { cast } = await fetchTvSeriesCredits(id);
+  const items = cast.slice(0, 10);
+
+  await setCacheItem(dynamoCacheKey, items, {
+    ttl: 43200, // 12 hours
+  });
+
+  return items;
+});
 
 export default async function Cast({
   className,
@@ -11,11 +30,7 @@ export default async function Cast({
   className?: string;
   id: number;
 }) {
-  'use cache';
-  cacheLife('days');
-
-  const { cast } = await fetchTvSeriesCredits(id);
-  const items = cast.slice(0, 10);
+  const items = await cachedCast(id);
 
   return items.length > 0 ? (
     <Avatars className={className} items={items.slice(0, 10)} />

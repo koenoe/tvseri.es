f3856d233a68be351270415de3a5bc2edf9b06ee
Koen Romers <koenoe@users.noreply.github.com>
2025-06-08 15:36:17 +0100

    Update KeywordsContainer.tsx

--- src/components/Keywords/KeywordsContainer.tsx
+++ src/components/Keywords/KeywordsContainer.tsx
@@ -1,15 +1,34 @@
+import { getCacheItem, setCacheItem } from '@/lib/db/cache';
 import { fetchTvSeriesKeywords } from '@/lib/tmdb';
+import { type Keyword as KeywordType } from '@/types/keyword';
 
 import Keyword from './Keyword';
+
+const cachedKeywords = async (id: string | number) => {
+  const dynamoCacheKey = `tv:keywords:${id}`;
+  const dynamoCachedItem = await getCacheItem<KeywordType[]>(dynamoCacheKey);
+  if (dynamoCachedItem) {
+    return dynamoCachedItem;
+  }
+
+  const results = await fetchTvSeriesKeywords(id);
+  const keywords = results ?? [];
+
+  await setCacheItem(dynamoCacheKey, keywords, {
+    ttl: 43200, // 12 hours
+  });
+
+  return keywords;
+};
 
 export default async function KeywordsContainer({
   tvSeriesId,
 }: Readonly<{
   tvSeriesId: number | string;
 }>) {
-  const keywords = await fetchTvSeriesKeywords(tvSeriesId);
+  const keywords = await cachedKeywords(tvSeriesId);
 
-  if (!keywords || keywords.length === 0) {
+  if (keywords.length === 0) {
     return null;
   }
 

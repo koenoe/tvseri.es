b2321737c9d2b1efc7b8057c4052251534c80dd8
Koen Romers <koenoe@users.noreply.github.com>
2025-06-14 23:02:35 +0100

    Update collections.ts

--- apps/api/src/routes/collections.ts
+++ apps/api/src/routes/collections.ts
@@ -1,6 +1,15 @@
 import { Hono } from 'hono';
 
-import { fetchTrendingTvSeries } from '@/lib/tmdb';
+import {
+  fetchApplePlusTvSeries,
+  fetchBestSportsDocumentariesTvSeries,
+  fetchKoreasFinestTvSeries,
+  fetchMostAnticipatedTvSeries,
+  fetchMostPopularTvSeriesThisMonth,
+  fetchPopularBritishCrimeTvSeries,
+  fetchTopRatedTvSeries,
+  fetchTrendingTvSeries,
+} from '@/lib/tmdb';
 
 const app = new Hono();
 
@@ -11,6 +20,83 @@ app.get('/trending', async (c) => {
     'Cache-Control',
     'public, max-age=28800, s-maxage=28800, stale-while-revalidate=3600',
   ); // 8h, allow stale for 1h
+
+  return c.json(items);
+});
+
+app.get('/top-rated', async (c) => {
+  const items = await fetchTopRatedTvSeries();
+
+  c.header(
+    'Cache-Control',
+    'public, max-age=604800, s-maxage=604800, stale-while-revalidate=3600',
+  ); // 1w, allow stale for 1h
+
+  return c.json(items);
+});
+
+app.get('/most-popular-this-month', async (c) => {
+  const items = await fetchMostPopularTvSeriesThisMonth();
+
+  c.header(
+    'Cache-Control',
+    'public, max-age=604800, s-maxage=604800, stale-while-revalidate=3600',
+  ); // 1w, allow stale for 1h
+
+  return c.json(items);
+});
+
+app.get('/most-anticipated', async (c) => {
+  const items = await fetchMostAnticipatedTvSeries();
+
+  c.header(
+    'Cache-Control',
+    'public, max-age=604800, s-maxage=604800, stale-while-revalidate=3600',
+  ); // 1w, allow stale for 1h
+
+  return c.json(items);
+});
+
+app.get('/koreas-finest', async (c) => {
+  const items = await fetchKoreasFinestTvSeries();
+
+  c.header(
+    'Cache-Control',
+    'public, max-age=2629800, s-maxage=2629800, stale-while-revalidate=3600',
+  ); // 1 month, allow stale for 1h
+
+  return c.json(items);
+});
+
+app.get('/must-watch-on-apple-tv', async (c) => {
+  const items = await fetchApplePlusTvSeries(c.req.query('region'));
+
+  c.header(
+    'Cache-Control',
+    'public, max-age=604800, s-maxage=604800, stale-while-revalidate=3600',
+  ); // 1w, allow stale for 1h
+
+  return c.json(items);
+});
+
+app.get('/best-sports-documentaries', async (c) => {
+  const items = await fetchBestSportsDocumentariesTvSeries();
+
+  c.header(
+    'Cache-Control',
+    'public, max-age=2629800, s-maxage=2629800, stale-while-revalidate=3600',
+  ); // 1 month, allow stale for 1h
+
+  return c.json(items);
+});
+
+app.get('/popular-british-crime', async (c) => {
+  const items = await fetchPopularBritishCrimeTvSeries();
+
+  c.header(
+    'Cache-Control',
+    'public, max-age=2629800, s-maxage=2629800, stale-while-revalidate=3600',
+  ); // 1 month, allow stale for 1h
 
   return c.json(items);
 });

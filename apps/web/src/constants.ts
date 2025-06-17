033383036d00ef5e8345d077ee0bcc8fd2976193
Koen Romers <koenoe@users.noreply.github.com>
2025-06-17 17:32:11 +0100

    Update constants.ts

--- apps/web/src/constants.ts
+++ apps/web/src/constants.ts
@@ -4,14 +4,6 @@ export const DEFAULT_BACKGROUND_COLOR = '#171717';
 export const DEFAULT_BACKGROUND_IMAGE =
   'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7';
 
-// TODO: eventually we'll just show the provider that users are subscribed to
-// but in order to do this we need a profile page or some way to store this information
-export const WATCH_PROVIDER_PRIORITY: Record<string, number> = {
-  'BBC iPlayer': -10,
-  Netflix: 0,
-  'Sky Go': 1000, // Sky Go is only available for Sky TV customers, so kinda sucks
-};
-
 export const DEFAULT_FETCH_RETRY_OPTIONS = {
   type: 'exponential',
   attempts: 4, // Initial + 3 retries

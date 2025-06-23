8259b225b80d245edc7eb89d08360277826610c8
Koen Romers <koenoe@users.noreply.github.com>
2025-06-23 13:43:08 +0100

    Update open-next.config.ts

--- apps/web/open-next.config.ts
+++ apps/web/open-next.config.ts
@@ -1,9 +1,6 @@
 import type { OpenNextConfig } from '@opennextjs/aws/types/open-next.js';
 
 const config = {
-  dangerous: {
-    enableCacheInterception: true,
-  },
   default: {
     override: {
       incrementalCache: 'multi-tier-ddb-s3',

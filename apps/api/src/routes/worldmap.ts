8cc19ea4130eb7cd429c345df356a5e9fc9bff0e
Koen Romers <koenoe@users.noreply.github.com>
2025-06-24 22:44:33 +0100

    Update worldmap.ts

--- apps/api/src/routes/worldmap.ts
+++ apps/api/src/routes/worldmap.ts
@@ -9,7 +9,10 @@ app.get('/', async (c) => {
     'https://gist.githubusercontent.com/koenoe/929074f4776bc8b3144a13076e3c158c/raw/d937831559314e40b43ec0add531177cbb9a5cb1/gistfile1.txt',
   );
 
-  c.header('Cache-Control', 'public, immutable, max-age=31536000'); // 1 year, immutable
+  c.header(
+    'Cache-Control',
+    'public, immutable, max-age=31536000, s-maxage=31536000',
+  ); // 1 year, immutable
 
   return c.json(data);
 });

9bba3e259149fed0565b6ef8d4e2bc497641ef97
Koen Romers <koenoe@users.noreply.github.com>
2025-06-25 22:21:47 +0100

    infra: add opennext as well to compare

--- infra/web.ts
+++ infra/web.ts
@@ -69,3 +69,65 @@ export const web = new vercel.Deployment('WebDeployment', {
 //     },
 //   });
 // }
+
+let openNextVersion: string | undefined;
+try {
+  openNextVersion =
+    require('../apps/web/package.json').devDependencies?.[
+      '@opennextjs/aws'
+    ]?.replace('^', '') ?? undefined;
+} catch {
+  openNextVersion = undefined;
+}
+
+new sst.aws.Nextjs('tvseries', {
+  openNextVersion,
+  domain: {
+    name: domain,
+    redirects: $app.stage === 'production' ? ['www.tvseri.es'] : [],
+    dns: sst.aws.dns({
+      zone,
+    }),
+  },
+  environment: {
+    // API_URL: apiRouter.url,
+    API_URL: 'https://api.tvseri.es',
+    API_KEY: secrets.apiKey.value,
+    OPEN_NEXT_FORCE_NON_EMPTY_RESPONSE: 'true',
+    SECRET_KEY: secrets.secretKey.value,
+    SITE_URL: `https://${domain}`,
+  },
+  imageOptimization: {
+    memory: '512 MB',
+    staticEtag: true,
+  },
+  path: 'apps/web',
+  server: {
+    architecture: 'arm64',
+    memory: '1 GB',
+    runtime: 'nodejs22.x',
+  },
+  warm: $app.stage === 'production' ? 3 : 0,
+  transform: {
+    server: {
+      timeout: '30 seconds',
+      nodejs: {
+        minify: true,
+        esbuild: {
+          external: ['@opennextjs/aws'],
+        },
+      },
+    },
+    cdn: (options) => {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const origins = (options.origins || []) as any[];
+      options.origins = origins.map((origin) => ({
+        ...origin,
+        originShield: {
+          enabled: true,
+          originShieldRegion: $app.providers?.aws.region ?? 'eu-west-2',
+        },
+      }));
+    },
+  },
+});

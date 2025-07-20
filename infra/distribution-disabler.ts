bd5e1e0c2c1987f0595505588b3248ae71ee9e5f
Koen Romers <koenoe@users.noreply.github.com>
2025-07-20 23:24:15 +0100

    Update distribution-disabler.ts

--- infra/distribution-disabler.ts
+++ infra/distribution-disabler.ts
@@ -60,7 +60,6 @@ export const killSwitchSnsTopic = new sst.aws.SnsTopic(
   {
     transform: {
       topic: {
-        name: `${$app.stage}-budget-alert-sns`,
         policy: $jsonStringify({
           Statement: [
             {

30287803b442d3aade1f464306d8055d73f061b9
Koen Romers <koenoe@users.noreply.github.com>
2025-09-15 21:25:09 +0100

    Update getHistoryKey.ts

--- apps/web/src/utils/getHistoryKey.ts
+++ apps/web/src/utils/getHistoryKey.ts
@@ -1,5 +1,5 @@
 export default function getHistoryKey(): string {
-  if (typeof window !== 'undefined' && window.history.state?.key) {
+  if (window?.history.state?.key) {
     return String(window.history.state.key);
   }
   return 'index';

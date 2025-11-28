2405bab61484927bf50c0c5ee8337cb565ba9264
Koen Romers <koenoe@users.noreply.github.com>
2025-11-28 20:01:33 +0000

    Update route.ts

--- apps/web/src/app/api/auth/session/route.ts
+++ apps/web/src/app/api/auth/session/route.ts
@@ -1,11 +1,10 @@
 import type { NextRequest } from 'next/server';
-import auth, { deleteSession } from '@/auth';
+import auth from '@/auth';
 
 export async function GET(req: NextRequest) {
   const session = await auth(req);
 
   if (!session.accessToken) {
-    await deleteSession();
     return Response.json({
       accessToken: null,
       expiresAt: null,

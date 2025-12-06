ecd1cb22a61366885bdf8fa94d41003f7b5db00f
Koen Romers <koenoe@users.noreply.github.com>
2025-12-06 21:25:23 +0000

    remove migration scripts

--- sst.config.ts
+++ sst.config.ts
@@ -77,6 +77,5 @@ export default $config({
     await import('./infra/api');
     await import('./infra/web');
     await import('./infra/distributionDisabler');
-    await import('./infra/migrateWatchedEpisodeData');
   },
 });

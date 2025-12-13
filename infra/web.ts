68e3a8254065b21f4291b8e17e7ab11d19785932
Koen Romers <koenoe@users.noreply.github.com>
2025-12-13 20:33:17 +0000

    bumps and cleanup

--- infra/web.ts
+++ infra/web.ts
@@ -27,25 +27,6 @@ new sst.aws.Nextjs('tvseries', {
     name: domain,
     redirects: $app.stage === 'production' ? ['www.tvseri.es'] : [],
   },
-  edge: {
-    viewerRequest: {
-      injection: $interpolate`
-          const uri = event.request.uri.toLowerCase();
-          const blockedPattern = /\.(php|asp|aspx|jsp|cgi|pl|py|sh|sql|env|config|bak|backup)(\?|$)/;
-
-          if (blockedPattern.test(uri)) {
-            return {
-              statusCode: 403,
-              statusDescription: 'Forbidden',
-              body: {
-                encoding: "text",
-                data: '<html><head><title>403 Forbidden</title></head><body><center><h1>403 Forbidden</h1></center></body></html>'
-              }
-            };
-          }
-        `,
-    },
-  },
   environment: {
     OPEN_NEXT_FORCE_NON_EMPTY_RESPONSE: 'true',
     SITE_URL: `https://${domain}`,

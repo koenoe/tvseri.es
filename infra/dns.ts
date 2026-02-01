36b393843a76342d77b48c5334ae4ef36d42c651
Koen Romers <koenoe@users.noreply.github.com>
2026-02-01 18:41:12 +0000

    fix: add CAA records to authorize AWS ACM certificate issuance
    AWS ACM was failing with CAA_ERROR when trying to issue SSL certificates
    for PR preview environments. Adding explicit CAA records to authorize
    Amazon certificate authorities.

--- infra/dns.ts
+++ infra/dns.ts
@@ -23,3 +23,24 @@ export const { zone, domain } = (() => {
     zone: AWS_HOSTED_ZONE_ID_DEV,
   };
 })();
+
+/**
+ * CAA (Certificate Authority Authorization) records
+ *
+ * These records explicitly authorize Amazon/AWS to issue SSL certificates
+ * for the domain. Required for ACM certificate validation to succeed.
+ *
+ * @see https://docs.aws.amazon.com/acm/latest/userguide/troubleshooting-caa.html
+ */
+new aws.route53.Record('CAARecord', {
+  name: domain,
+  records: [
+    '0 issue "amazon.com"',
+    '0 issue "amazontrust.com"',
+    '0 issue "awstrust.com"',
+    '0 issue "amazonaws.com"',
+  ],
+  ttl: 300,
+  type: 'CAA',
+  zoneId: zone,
+});
